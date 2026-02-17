e7be8d490bf3f86a1442fc5bb5fc2f5b63075a54
Repo Initializer <repo@example.com>
2026-02-18 00:07:40 +0530

    Fix comment syntax in chapter_02_Java_Concepts/05_core_comments_JS.js

---
+++ chapter_02_Java_Concepts/05_core_comments_JS.js
@@ -0,0 +1,9 @@
+
+// this code won't be executed, it's just for demonstration purposes.
+console.log("This is a single-line comment.");
+
+/*
+This is a multi-line comment.
+It can span multiple lines.
+*/
+
